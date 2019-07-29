025e973db3d48c98b4b7fd27f7e371d2546897e8
mariourban83 <mario.urban.83@gmail.com>
2019-07-29 18:22:14 +0100

    Add buildSelect country function + display data function

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,5 +8,31 @@ fetch(url1).then(function (res) {
   return res.json()
 }).then(function (data) {
   dataCountries = data;
+  buildCountrySelect(data);
   console.log(data);
 })
+
+// Build SELECT element with OPTIONS on webpage for country selection
+function buildCountrySelect(b) {
+  let select = document.createElement('select');
+  b.forEach(function (item, index) {
+    let option = document.createElement('option');
+    option.value = index;
+    option.textContent = item.name;
+    select.appendChild(option);
+  })
+  select.addEventListener("change", displayData);
+  document.querySelector('#pickCountry').appendChild(select);
+}
+
+//Variables for storing  & Function for inserting data into html elements on the webpage
+function displayData(d) {
+  const countryName = document.querySelector("#countryName");
+  const countryCapital = document.querySelector("#countryCapital");
+  const countryPopulation = document.querySelector("#countryPopulation");
+  let obj = dataCountries[d.target.value];
+  countryName.innerHTML = '<h4>' + 'Country Name : ' + obj.name + '</h4>';
+  countryCapital.innerHTML = '<h4>' + 'Country Capital : ' + obj.capital + '</h4>';
+  countryPopulation.innerHTML = '<h5>' + 'Current Population : ' + obj.population + '</h5>';
+  document.querySelector('#countryFlag').src = obj.flag;
+}
