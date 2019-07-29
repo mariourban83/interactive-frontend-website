f41f1d320675b740283bb1fa7ab171798bced41c
mariourban83 <mario.urban.83@gmail.com>
2019-07-29 20:00:13 +0100

    Completed section landing view, small changes to styling and path structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,9 +30,16 @@ function displayData(d) {
   const countryName = document.querySelector("#countryName");
   const countryCapital = document.querySelector("#countryCapital");
   const countryPopulation = document.querySelector("#countryPopulation");
+  const countryRegion = document.querySelector("#countryRegion");
+  const countrySubregion = document.querySelector("#countrySubregion");
+  const countryTimezone =  document.querySelector("#countryTimezone");
   let obj = dataCountries[d.target.value];
-  countryName.innerHTML = '<h4>' + 'Country Name : ' + obj.name + '</h4>';
-  countryCapital.innerHTML = '<h4>' + 'Country Capital : ' + obj.capital + '</h4>';
+  //countryOnMap(obj.latLng);
+  countryName.innerHTML = '<h5>' + 'Country Name : ' + obj.name + '</h5>';
+  countryCapital.innerHTML = '<h5>' + 'Country Capital : ' + obj.capital + '</h5>';
+  countryRegion.innerHTML = '<h5>' + 'Country Region : ' + obj.region + '</h5>';
+  countrySubregion.innerHTML = '<h5>' + 'Country Subregion : ' + obj.subregion + '</h5>';
   countryPopulation.innerHTML = '<h5>' + 'Current Population : ' + obj.population + '</h5>';
+  countryTimezone.innerHTML = '<h5>' + 'Timezone(s) : ' + obj.timezones + '</h5>';
   document.querySelector('#countryFlag').src = obj.flag;
 }
