7fd07b807923c51aaaf7adf4e0a1d476fb3d8126
Mario Urban <49397106+mariourban83@users.noreply.github.com>
2020-12-14 14:40:31 +0000

    Updated token for mapbox access

--- assets/js/leafletMap.js
+++ assets/js/leafletMap.js
@@ -7,5 +7,5 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoibWFyaW91cmJhbiIsImEiOiJjanlvajR0eHUwOHZhM2dwZHkxcHA1aXg2In0.gL34DtbkN1pUVabRV93qOw',
+    accessToken: 'pk.eyJ1IjoibWFyaW91cmJhbiIsImEiOiJja2lvbnh3cG4wa2drMnpveWNsNWs5cTRxIn0.u3elN3N2NH32dHY7PWDZXg',
 }).addTo(mymap);
